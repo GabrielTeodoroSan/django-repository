98ddba7d26173453b573229b0f73494ca03ded4f
GabrielTedoroSan <gabrieltedorodomingues@gmail.com>
2022-07-20 10:35:25 -0300

    First HttpRequest and Response

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def my_view(request):
+    return HttpResponse('HELLO, DJANGO!!!')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
