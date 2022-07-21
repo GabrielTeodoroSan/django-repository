e8116832913b56acf3d10108463665717d51335d
GabrielTedoroSan <gabrieltedorodomingues@gmail.com>
2022-07-21 09:52:40 -0300

    add more views and urls in urls.py

--- project/urls.py
+++ project/urls.py
@@ -17,10 +17,20 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def my_view(request):
-    return HttpResponse('HELLO, DJANGO!!!')
+def home(request):
+    return HttpResponse('HOME!!!')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE!!!')
+
+
+def contato(request):
+    return HttpResponse('CONTATO!!!')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view)
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
 ]
