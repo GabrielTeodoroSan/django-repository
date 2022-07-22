a6217407588cb1f6d63e32f820cd94fd384b3961
GabrielTeodoroSan <gabrielteodorodomingues@gmail.com>
2022-07-22 11:02:40 -0300

    Removing contato and sobre views and contato template.

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('sobre/', views.sobre),
-    path('contato/', views.contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,9 +5,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html', status=201, context={'name': 'Gabriel Teodoro'})
-
-def sobre(request):
-    return HttpResponse('SOBRE!!!')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
