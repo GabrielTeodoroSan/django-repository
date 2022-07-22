03bd59b613062a726512e11505f19241a4b374e3
GabrielTeodoroSan <gabrielteodorodomingues@gmail.com>
2022-07-22 12:04:14 -0300

    Creating some folder to template parts.

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html', status=201, context={'name': 'Gabriel Teodoro'})
+    return render(request, 'recipes/pages/home.html', status=201, context={'name': 'Gabriel Teodoro'})
